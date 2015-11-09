e02a2e0d5b8f7fd73e37a24b6e6d8240863191d6
richard.ferraresi@modinify.com <richard.ferraresi@modinify.com>
2015-11-09 18:36:56 -0200

    removed ide/platform info and rebuilded plugin from "Hello" to openActivity

---
+++ src/android/src/com/modinify/openactivity/openActivity.java
@@ -0,0 +1,27 @@
+package com.modinify.openactivity;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+import android.content.Context;
+import android.content.Intent;
+
+public class openActivity extends CordovaPlugin{
+
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Context context=this.cordova.getActivity().getApplicationContext();
+        boolean isOk = false;
+
+        try{
+            Class<?> c = Class.forName(action);
+            Intent intent=new Intent(context, c);
+            cordova.getActivity().startActivity(intent);
+            isOk = true;
+        }catch(Exception e){
+            // DO nothing for while...
+        }
+
+        return isOk;
+    }
+
+}
---
+++ www/openactivity.js
@@ -0,0 +1,18 @@
+/*global cordova, module*/
+
+cordova.define("com.modinify.openactivity", function(require, exports, module) {
+    var argscheck = require('cordova/argscheck'),
+        utils = require('cordova/utils'),
+        exec = require('cordova/exec'),
+        channel = require('cordova/channel');
+
+    var OpenActivity = function() {
+    };
+
+    OpenActivity.open = function(name) {
+        exec(null, null, "openActivity", name, []);
+    };
+
+    module.exports = OpenActivity;
+});
+
