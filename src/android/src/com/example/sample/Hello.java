476a4c6fab5202283275623d373c67882adf60bf
richard.ferraresi@modinify.com <richard.ferraresi@modinify.com>
2015-11-09 17:57:55 -0200

    general adjusts

--- src/android/src/com/example/sample/Hello.java
+++ src/android/src/com/example/sample/Hello.java
@@ -5,6 +5,9 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 
 import android.util.Log;
 
@@ -13,6 +16,37 @@ import com.example.sample.MainActivity;
 public class Hello extends CordovaPlugin
 {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    	Context context=this.cordova.getActivity().getApplicationContext();
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+		// set title
+		alertDialogBuilder.setTitle("BNP");
+
+		// set dialog message
+		alertDialogBuilder
+				.setMessage("Click yes to exit!")
+				.setCancelable(false)
+				.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,int id) {
+						// if this button is clicked, close
+						// current activity
+						//MainActivity.this.finish();
+					}
+				})
+				.setNegativeButton("No", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						// if this button is clicked, just close
+						// the dialog box and do nothing
+						dialog.cancel();
+					}
+				});
+
+		// create alert dialog
+		AlertDialog alertDialog = alertDialogBuilder.create();
+
+		// show it
+		alertDialog.show();
+
 		return true;
 	}
 
