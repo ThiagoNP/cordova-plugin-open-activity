db2f756293c9abc36923d52310969a68af063c49
richard.ferraresi@modinify.com <richard.ferraresi@modinify.com>
2015-11-09 17:45:42 -0200

    general adjusts

--- src/android/src/com/example/sample/Hello.java
+++ src/android/src/com/example/sample/Hello.java
@@ -13,26 +13,7 @@ import com.example.sample.MainActivity;
 public class Hello extends CordovaPlugin
 {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		if (action.equals("open")) {
-				try {
-					openN();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			else {
-			}
-			return true;
-	}
-	private void openN() throws IOException {
-
-		intent = new Intent(this.cordova.getActivity().getApplicationContext(), MainActivity.class);
-
-		this.cordova.getActivity().startActivityForResult(intent,0);
-		this.cordova.getActivity().startActivity(intent);
-		this.cordova.getActivity().overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+		return true;
 	}
 
-	}
 }
